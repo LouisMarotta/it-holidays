35e4ca23f6a034cf3b469b17c88e7edc9dcf78bf
Louis Marotta <38915875+LouisMarotta@users.noreply.github.com>
2023-07-06 16:52:08 +0200

    Try to remove php-calendar as requirement

--- src/ITHolidays/Traits/Holidays/EasterMonday.php
+++ src/ITHolidays/Traits/Holidays/EasterMonday.php
@@ -13,10 +13,32 @@ trait EasterMonday
      */
     private function setEasterMonday(int $year)
     {
-        $date = Carbon::create($year, 3, 21, 0, 0, 0);
-        $days = easter_days($year);
+        $days = $this->calculateEasterDate($year);
+
+        $date = Carbon::create($year, $days["month"], $days["day"], 0, 0, 0);
+        //$days = easter_days($year);
 
         return $date->addDays($days + 1);
+    }
+    
+    function calculateEasterDate($year)
+    {
+        $a = $year % 19;
+        $b = (int) ($year / 100);
+        $c = $year % 100;
+        $d = (int) ($b / 4);
+        $e = $b % 4;
+        $f = (int) (($b + 8) / 25);
+        $g = (int) (($b - $f + 1) / 3);
+        $h = (19 * $a + $b - $d - $g + 15) % 30;
+        $i = (int) ($c / 4);
+        $k = $c % 4;
+        $l = (32 + 2 * $e + 2 * $i - $h - $k) % 7;
+        $m = (int) (($a + 11 * $h + 22 * $l) / 451);
+        $month = (int) (($h + $l - 7 * $m + 114) / 31);
+        $day = ($h + $l - 7 * $m + 114) % 31 + 1;
+        
+        return ["year" => $year, "month" => $month, "day" => $day];
     }
 
     /**
